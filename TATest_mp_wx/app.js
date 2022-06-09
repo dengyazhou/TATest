137c30484e9575516c7a57271e15e1c98703307c
yazhou.xm <yazhou.deng@ximalaya.com>
2022-06-09 17:07:33 +0800

    TA接入微信小程序 mp_wx

--- TATest_mp_wx/app.js
+++ TATest_mp_wx/app.js
@@ -1,4 +1,27 @@
 // app.js
+
+// 引入SDK
+var ThinkingAnalyticsAPI = require("./thinkingdata.wx.min.js");
+
+// TA SDK 配置对象
+var config = {
+  appid: "9ea29e5b57e6440381454bb6681b5e84",
+  serverUrl: "https://receiver-ta-dev.thinkingdata.cn",
+  // autoTrack: {
+  //   appLaunch: true, // 自动采集 ta_mp_launch
+  //   appShow: true, // 自动采集 ta_mp_show
+  //   appHide: true, // 自动采集 ta_mp_hide
+  //   pageShow: true, // 自动采集 ta_mp_view
+  //   pageShare: true // 自动采集 ta_mp_share
+  // }
+};
+
+// 创建 TA 实例
+var ta = new ThinkingAnalyticsAPI(config);
+
+// 初始化
+ta.init();
+
 App({
   onLaunch() {
     // 展示本地存储能力
@@ -10,6 +33,31 @@ App({
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        
+        // 小程序开始
+        console.log("hello world 邓亚洲");
+      
+        // 2.1 设置访客 ID
+        // ta.identify("your_own_anonymous_id");
+        // 2.2 设置账号 ID
+        // ta.login("ABC_123456");
+        // 2.3 设置公共事件属性
+        // 2.3.1 设置事件公共属性
+        // ta.setSuperProperties( { "channel": "渠道"} );
+        // ta.setSuperProperties( {channel1: "渠道1"} );
+        // 清除属性名为 CHANNEL 的公共属性
+        // ta.unsetSuperProperty("channel1");
+        // 清除公共事件属性
+        // ta.clearSuperProperties();
+        // console.log(ta.getSuperProperties());
+        // 2.3.2 设置动态公共属性
+        // ta.setDynamicSuperProperties( () => {
+        //   return {"channel2": "渠道3"};
+        // });
+
+        // 上报一条数据
+        ta.track("test_event_13");
+
       }
     })
   },
